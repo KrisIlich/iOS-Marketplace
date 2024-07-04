d8201480b20c12a9b0d174efb68070485d31637d
Kristopher Ilich <kristopherilich@gmail.com>
2024-07-04 10:36:46 -0400

    Initial Commit

---
+++ heirloom/heirloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  heirloomApp.swift
+//  heirloom
+//
+//  Created by Kristopher on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct heirloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
